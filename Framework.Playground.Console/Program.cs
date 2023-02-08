d5c6320d81df60b499b5a4c46ad6c00e0fe0b39a
a.ahmadi <a.ahmadi@hoshmand.local>
2023-02-08 11:10:20 +0330

    Add Code Sample

--- Framework.Playground.Console/Program.cs
+++ Framework.Playground.Console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Framework.Logging;
+using Framework.Logging.Console.Extensions;
+using Framework.Logging.Elastic.Extensions;
+using Framework.Logging.Files.Extensions;
+
+var logBuilder = new LogBuilder()
+    .MinimumLevel.Debug()
+    .DeleteBefore.Week(2)
+    .DateTime.Set(false)
+    .WriteTo.File(rollingInterval: Framework.Logging.RollingInterval.Day)
+    .WriteTo.ElasticSearch("http://127.0.0.1:9200/", "elastic", "elastic")
+    .WriteTo.Console()
+    .CreateLogger();
+
+logBuilder.Info("Logger Test", "1", "2", "3");
+var logs = logBuilder.Read(DateTime.Now.Date);
+
+foreach (var log in logs)
+{
+    Console.WriteLine(log);
+}
+
+Console.ReadKey();
